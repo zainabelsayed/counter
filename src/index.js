f62c3dbc3f3ee539d8382651ab5d7858ceebcba6
2871992z <eng.zainababdo@gmail.com>
2021-10-22 22:35:35 +0200

    adding validation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import { Provider }from 'react-redux'
-import { store } from './components/store/todoStore'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Provider } from "react-redux";
+import { store } from "./components/store/todoStore";
 
 ReactDOM.render(
-  <React.StrictMode store={store}>
-    <Provider>
-    <App />
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
