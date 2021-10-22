1b9cd07ab07c2011e0b8eed059adb258048fa7e4
2871992z <eng.zainababdo@gmail.com>
2021-10-22 22:23:46 +0200

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
-import Counter from "./components/counter/Counter";
-import Shop from "./components/e-commerce/Shop";
+import Counter from "./components/Counter";
 import NavBar from "./components/navbar/NavBar";
-import Home from "./components/home/Home";
-import ProductDetails from "./components/e-commerce/ProductDetails";
-import Todo from "./components/todo/Todo";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
@@ -15,11 +11,7 @@ function App() {
       <Router>
       <NavBar />
         <Switch>
-          <Route path="/" component={Home} exact/>
           <Route path="/counter" component={Counter} />
-          <Route path="/shop" component={Shop} exact/>
-          <Route path="/shop/:id" component={ProductDetails}/>
-          <Route path="/todo" component={Todo}/>
           <Route path="/login" component={Login}/>
           <Route path="/register" component={Register}/>
         </Switch>
